42733de93b9fd85fa4fdb4f6eb9f731d9d5acef0
Aravind Ramalingam <ramaliar@amazon.com>
2022-04-27 23:33:48 -0700

    Adding spec and status for BMCJob type

--- api/v1alpha1/bmcjob_types.go
+++ api/v1alpha1/bmcjob_types.go
@@ -20,26 +20,88 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
-// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
+// BMCJobConditionType represents the condition of the BMC Job.
+type BMCJobConditionType string
+
+const (
+	// JobCompleted represents successful completion of the BMC Job tasks.
+	JobCompleted BMCJobConditionType = "Completed"
+	// JobFailed represents failure in BMC job execution.
+	JobFailed BMCJobConditionType = "Failed"
+	// JobTaskCompleted represents successful completion of a BMC Job task.
+	JobTaskCompleted BMCJobConditionType = "TaskCompleted"
+)
+
 // NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
 
 // BMCJobSpec defines the desired state of BMCJob
 type BMCJobSpec struct {
-	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
-	// Important: Run "make" to regenerate code after modifying this file
+	// Tasks represents a list of baseboard management actions to be executed.
+	// The tasks are completed in order.
+	Tasks []Task `json:"tasks"`
+}
 
-	// Foo is an example field of BMCJob. Edit bmcjob_types.go to remove/update
-	Foo string `json:"foo,omitempty"`
+// Task represents the action to be performed.
+// A single task can only perform one type of action.
+// For example either PowerAction or BootAction.
+// +kubebuilder:validation:MaxProperties:=1
+type Task struct {
+	// PowerAction represents a baseboard management power operation.
+	PowerAction *PowerAction `json:"powerAction,omitempty"`
+
+	// BootAction represents a baseboard management boot device operation.
+	BootAction *BootAction `json:"bootAction,omitempty"`
+}
+
+type PowerAction struct {
+	// State represents the requested power state to set for the baseboard management.
+	// +kubebuilder:validation:Enum=On;Off;Status;Cycle;Reset;Soft
+	State PowerState `json:"state"`
+}
+
+type BootAction struct {
+	// BootDevice represents the requested boot device to set.
+	// +kubebuilder:validation:Enum=PXE;Disk;BIOS;CDROM;Safe
+	BootDevice BootDevice `json:"bootDevice"`
+
+	// Persistent if True, boot device is permanently set for the baseboard management.
+	// If False, boot device is set as one time boot.
+	// +kubebuilder:default=false
+	Persistent bool `json:"persistent,omitempty"`
+
+	// EfiBoot specifies to EFI boot for the baseboard management.
+	// +kubebuilder:default=false
+	EfiBoot bool `json:"efiBoot,omitempty"`
 }
 
 // BMCJobStatus defines the observed state of BMCJob
 type BMCJobStatus struct {
-	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
-	// Important: Run "make" to regenerate code after modifying this file
+	// Conditions represents the latest available observations of an object's current state.
+	// +optional
+	Conditions []BMCJobCondition `json:"conditions,omitempty"`
+
+	// StartTime represents time when the BMCJob controller started processing a job.
+	// +optional
+	StartTime *metav1.Time `json:"startTime,omitempty"`
+
+	// CompletionTime represents time when the job was completed.
+	// The completion time is only set when the job finishes successfully.
+	// +optional
+	CompletionTime *metav1.Time `json:"completionTime,omitempty"`
+}
+
+type BMCJobCondition struct {
+	// Type of the BMCJob condition.
+	Type BMCJobConditionType `json:"type"`
+
+	// Message represents human readable message indicating details about last transition.
+	// +optional
+	Message string `json:"message,omitempty"`
 }
 
 //+kubebuilder:object:root=true
 //+kubebuilder:subresource:status
+//+kubebuilder:resource:path=bmcjobs,scope=Namespaced,categories=tinkerbell,singular=bmcjob,shortName=bmj
 
 // BMCJob is the Schema for the bmcjobs API
 type BMCJob struct {
